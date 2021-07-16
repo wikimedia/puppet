mathewjose251/puppet-1

mariadb104-test: stack-specific changes.
Change-Id: I4db311eab8030ccf2d0afe9985d6b1956a3c7ee7

modules/profile/manifests/mariadb/ferm.pp
@@ -23,6 +23,12 @@ define profile::mariadb::ferm (
         notrack => true,
         srange  => '$INTERNAL',
     }
+    ferm::service{ "${rule_name}_labs":
+        proto   => 'tcp',
+        port    => $port,
+        notrack => true,
+        srange  => '172.16.0.0/12',
+    }
 
     # auxiliary port
     if $port == '3306' {

wikimedia/puppet

modules/profile/manifests/mariadb/ferm.pp
@@ -29,6 +29,12 @@ define profile::mariadb::ferm (
         notrack => true,
         srange  => '@resolve((dborch1001.wikimedia.org))',
     }
+    ferm::service{ "${prefix}mariadb_internal_labs":
+        proto   => 'tcp',
+        port    => $port,
+        notrack => true,
+        srange  => '172.16.0.0/12',
+    }
 
     # auxiliary port
     if $port == '3306' {
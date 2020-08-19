8a214b1853767256b2d58f84787e7d13cf9b430f
Stephen Shirley <sshirley@wikimedia.org>
2020-08-19 11:15:14 +0200

    mariadb104-test: stack-specific changes.
    Change-Id: I4db311eab8030ccf2d0afe9985d6b1956a3c7ee7

--- modules/role/manifests/cluster/management.pp
+++ modules/role/manifests/cluster/management.pp
@@ -16,10 +16,10 @@ class role::cluster::management {
     # but Spicerack/Cumin/Homer should be run from cumin2002 to prevent
     # issues with the Homer repo sync
     # lint:ignore:wmf_styleguide
-    unless $::fqdn == 'cumin2001.codfw.wmnet' {
-        include profile::spicerack
-        include profile::homer
-    }
+    #unless $::fqdn == 'cumin2001.codfw.wmnet' {
+    #    include profile::spicerack
+    #    include profile::homer
+    #}
     # lint:endignore
 
     include profile::cumin::master
@@ -32,15 +32,15 @@ class role::cluster::management {
     include profile::pwstore
 
     include profile::mariadb::wmf_root_client
-    include profile::dbbackups::transfer
+    #include profile::dbbackups::transfer
 
-    include profile::netops::ripeatlas::cli
+    #include profile::netops::ripeatlas::cli
 
     include profile::sre::os_updates
     include profile::sre::check_user
 
     # Backup all of /srv, including deployment, homer and  pwstore
     # move to a corresponding profile if the other profiles are split away
-    include profile::backup::host
-    include profile::cluster::management::backup
+    #include profile::backup::host
+    #include profile::cluster::management::backup
 }
